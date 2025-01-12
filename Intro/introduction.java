cd6b600c8932aa45331530aa09b7b38e29ee3523
Pranto Kumar <prantokumar.net@gmail.com>
2025-01-12 11:47:51 +0600

    introduction of java

---
+++ Intro/introduction.java
@@ -0,0 +1,25 @@
+package Intro; // package name or folder container
+public class introduction { // class name should not be same as file name
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to jaava programming programming.");
+        System.out.println("Today Day 01 In java programming.!");
+        
+        int a = 10;// initialization;
+      
+        int b; //declearation;
+        b = 50; // Assignment;
+        
+        System.out.println(a);
+        System.out.println(b);
+
+        boolean z = false;
+        System.out.println(z);
+
+        char c = 'P';
+        System.out.println(c);
+
+        String s = "PRITOM";
+        System.out.println(s);
+    }
+}
