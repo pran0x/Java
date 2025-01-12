348f26a16ce13ad1a4c0b9ce376793c062b2e22c
Pranto Kumar <prantokumar.net@gmail.com>
2025-01-12 12:18:41 +0600

    sysout added & fixs
    System input output, scanner buffer fix, Resource Leak fix added

--- Intro/introduction.java
+++ Intro/introduction.java
@@ -1,4 +1,7 @@
 package Intro; // package name or folder container
+
+import java.util.Scanner; // import Scanner class from java.util package;
+
 public class introduction { // class name should not be same as file name
     public static void main(String[] args){
         System.out.println("Hello, World!");
@@ -21,5 +24,47 @@ public class introduction { // class name should not be same as file name
 
         String s = "PRITOM";
         System.out.println(s);
+
+
+        String s1 = "PRITOM";
+        String s2 = "PRANTO";
+
+        System.out.println(s1 + " " + s2);
+        // s1 = s2;
+        System.out.println("S1 : " + s1);
+        System.out.println("S2 : " + s2);
+        String temp = s1;
+        s1 = s2;
+        s2 = temp;
+        // Aftr Swap;
+        System.out.println("s1 : " + s1);
+        System.out.println("s2 : " + s2);
+
+
+        //user Input;
+        try(Scanner scanner = new Scanner(System.in)){ // input mathod;
+          // Using try catch for avoid Resource Leak;
+          /* Method:
+              try( resource){
+              !code;
+              }
+           */
+        System.out.print("Enter Your Name : ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+
+        System.out.print("Enter your age : ");
+        int age = scanner.nextInt();
+        System.out.println("You are " + age + " years old.");
+
+        System.out.println("What is your favorite food ?  : ");
+        scanner.nextLine(); // to clear the  scanner buffer;
+        String food = scanner.nextLine();
+
+        System.out.println("Your Details : " + name + " " + age + " ");
+        System.out.println("Your Favorite Food is  : " + food);
+
+        scanner.close(); // For avoid Resource leak; 
+        }
     }
 }
