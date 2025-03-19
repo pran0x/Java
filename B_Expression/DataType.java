32c847456e68d16f995e66d330a22727bc6c1c44
pran0x <prantokumar.net@gmail.com>
2025-03-20 02:08:25 +0600

    Math Expression added

--- B_Expression/DataType.java
+++ B_Expression/DataType.java
@@ -35,5 +35,25 @@ public class DataType {
     System.out.println("Right shift operation (b = 10): " + (b >> 2));
 
     // #------------------------------//#endregion
+    int N1 = 6 - 3 - 3;
+    int N2 = 6 - ( 3 - 3 );
+    double N3 = 3 +  5 / 2;
+    double N4 = 5 / 2.0f;
+    double N5 = 5.0f / ( 6 - 4 % 6 );
+    double N6 = 2- 10 % 4;
+    double N7 = 100 / 5 / 2 % 7;
+    double N8 = ( 1 + 5 ) % 3 * 2 - 2 ;
+
+    System.out.println("N1 = " + N1);
+    System.out.println("N2 = " + N2);
+    System.out.println("N3 = " + N3);
+    System.out.println("N4 = " + N4);
+    System.out.println("N5 = " + N5);
+    System.out.println("N6 = " + N6);
+    System.out.println("N7 = " + N7);
+    System.out.println("N8 = " + N8);
+    if(1%3 == 0){
+      System.out.println("1%3 == 0");
+    }
   }
 }
